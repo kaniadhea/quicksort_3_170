472bbbcb10f43336cb7404b1cafdce0ea7664a33
DELL <DELL@DESKTOP-KLJI07S>
2023-03-31 17:23:52 +0700

    index x dan index y

--- quicksort_3_170/quicksort_3_170.cpp
+++ quicksort_3_170/quicksort_3_170.cpp
@@ -13,11 +13,20 @@ void input() {
 		cin >> n; 
 
 		if (n <= 20)
-			break
-		else
+			break;
+		else 
 			cout << "\nMaksimum panjang array adalah 20" << endl; 
 
 	}
 
+	cout << "\n========================" << endl;
+	cout << "\nEnter array element" << endl; 
+	cout << "\n========================" << endl; 
 
+	for (int i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
 }
+// swap the element at index x with the element at index y 
