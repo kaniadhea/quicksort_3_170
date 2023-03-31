db76db790d1f5a9323e3835903d3f550a058ca8a
DELL <DELL@DESKTOP-KLJI07S>
2023-03-31 18:08:31 +0700

    display

--- quicksort_3_170/quicksort_3_170.cpp
+++ quicksort_3_170/quicksort_3_170.cpp
@@ -96,6 +96,17 @@ void display() {
 		cout << arr[i] << " ";
 	}
 	cout << "\n\nNumber of comparasions: " << cmp_count << endl;
+	cout << "Number of data movemen: " << mov_count << endl;
+}
+
+int main() {
+	input();
+	//sort the array using quick sort 
+	q_short(0, n - 1);
+	display();
+	system("pause");
+
+	return 0;
 
 }
 
