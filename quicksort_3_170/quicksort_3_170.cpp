0aa45824b7e4750cff42cfc60d12628806b4d4a2
DELL <DELL@DESKTOP-KLJI07S>
2023-03-31 17:17:10 +0700

    maksimum panjang array

---
+++ quicksort_3_170/quicksort_3_170.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std; 
+
+//array of integrers to hold values
+int arr[20];
+int cmp_count = 0; // number of comprasion
+int mov_count = 0; // number of movement 
+int n;
+void input() {
+	while (true)
+	{
+		cout << "Masukkan panjang element array :";
+		cin >> n; 
+
+		if (n <= 20)
+			break
+		else
+			cout << "\nMaksimum panjang array adalah 20" << endl; 
+
+	}
+
+
+}
